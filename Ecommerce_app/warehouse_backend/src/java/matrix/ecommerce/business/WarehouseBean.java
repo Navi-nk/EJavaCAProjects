fbb2a138660579cb53f18f643d5af5056d7033f1
Navi <naval.kumar99@gmail.com>
2017-11-01 14:50:32 +0800

    added json code

--- Ecommerce_app/warehouse_backend/src/java/matrix/ecommerce/business/WarehouseBean.java
+++ Ecommerce_app/warehouse_backend/src/java/matrix/ecommerce/business/WarehouseBean.java
@@ -7,6 +7,8 @@ package matrix.ecommerce.business;
 
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -27,12 +29,20 @@ import javax.jms.TextMessage;
 )
 
 public class WarehouseBean implements MessageListener {
+    
+    
+    @Inject
+    Event<String> jmsEvent;
+     
+     
     @Override
 	public void onMessage(Message message) {
 		TextMessage txtMsg = (TextMessage)message;
 		System.out.println("... received");
 		try {
+                        System.out.println("inside jms");
 			System.out.println("\t" + txtMsg.getText());
+                         jmsEvent.fire(txtMsg.getText());
 		} catch (JMSException ex) {
 			ex.printStackTrace();
 		}
