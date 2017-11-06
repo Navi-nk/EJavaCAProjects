87b2934621e02676ef9a25023abbb4c932e568a0
Sarita Sethy <e0146848@u.nus.edu>
2017-11-06 14:15:45 +0800

    new features

--- SnapNShare/src/java/matrix/snapshot/business/UserBean.java
+++ SnapNShare/src/java/matrix/snapshot/business/UserBean.java
@@ -1,8 +1,13 @@
 package matrix.snapshot.business;
 
+import java.util.LinkedList;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import matrix.snapshare.model.User;
+import matrix.snapshare.model.UserFriend;
 
 /**
  *
@@ -24,9 +29,32 @@ public class UserBean {
          
     }
     
-    public void getFriends() 
+    public List<String> getFriends(String name) 
     {
-            //em.persist();
+        List<String> friends = new LinkedList<>();
+       
+        String queryString = "SELECT u FROM User u where u.userName=:name";
+        TypedQuery<User> query =  em.createQuery(queryString, User.class);
+        query.setParameter("name", name);
+         User user = query.getSingleResult();
+        
+        queryString = "SELECT u FROM UserFriend u where u.user=:name";
+        TypedQuery<UserFriend> query1 =  em.createQuery(queryString, UserFriend.class);
+        query1.setParameter("name", user);
+        
+        List<UserFriend> list = query1.getResultList();
+        if (list == null || list.isEmpty()) {
+         return null;
+        }
+        else
+        {
+            for(UserFriend friend:list)
+            {
+                friends.add(friend.getUserFriend().getUserName());
+            }
+        }
+            
+        return friends;
          
     }
 }
