a44637845ca5e7c39ea7b5293736654754df96ec
Sarita Sethy <e0146848@u.nus.edu>
2017-11-06 16:04:06 +0800

    Added retrival of timeline photos

--- SnapNShare/src/java/matrix/snapnshare/rest/Timeline.java
+++ SnapNShare/src/java/matrix/snapnshare/rest/Timeline.java
@@ -1,11 +1,19 @@
 package matrix.snapnshare.rest;
 
+import java.util.List;
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import matrix.snapshare.model.Image;
+import matrix.snapshot.business.TimeLineBean;
 
 /**
  *
@@ -14,12 +22,27 @@ import javax.ws.rs.core.MediaType;
 @RequestScoped
 @Path("/timeline")
 public class Timeline {
-    
+    @EJB TimeLineBean timeLineBean;
     @GET
+    @Path("{username}")
     @Produces(MediaType.APPLICATION_JSON)
-    public void retrieveTimeLine(@PathParam("username") String name) {
-        
-        
+    public Response retrieveTimeLine(@PathParam("username") String name) {
+       JsonArrayBuilder jsonArray=Json.createArrayBuilder();
+       if(name!=null)
+           {
+               List<Image> imageList= timeLineBean.getImages(name);
+               for(Image image:imageList)
+               {
+                   JsonObjectBuilder imageBuilder=Json.createObjectBuilder();
+                   imageBuilder.add("postedBy", image.getUser().getUserName());
+                   imageBuilder.add("url", image.getImageUrl());
+                   imageBuilder.add("comments", image.getComments());
+                   imageBuilder.add("postTime", image.getCraetedDate().toString());
+                   jsonArray.add(imageBuilder);
+               }
+           }
+           
+        return  Response.ok(jsonArray.build()).build();
     
     
     }
